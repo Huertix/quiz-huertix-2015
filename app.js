98bcb2993839fd9db7dd30f7d5afb9d11d19c277
David Huerta <dhuerta23@gmail.com>
2015-07-13 23:21:53 +0200

    Layout Introducido

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ var routes = require('./routes/index');
 
 var app = express();
 
-app.use('/', routes);
+//app.use('/', routes);
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
