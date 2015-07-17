6490f0cc003d668d31d51909b3b5d400518347d9
David Huerta <dhuerta23@gmail.com>
2015-07-17 22:54:58 +0200

    Funcion control errores Load

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -1,11 +1,21 @@
 var models = require('../models/models.js');
 
-
+exports.load = function(req, res, next, quizId){
+	models.Quiz.findById(quizId).then(
+		function(quiz){
+			if(quiz){
+				req.quiz = quiz;
+				next();
+			}else{
+				next (new Error('No existe quizId ' + quizId));
+			}
+		}).catch(function(error){ next(error);});
+};
 
 exports.index = function(req, res) {
 	models.Quiz.findAll().then(function(quizes) {
 		res.render('quizes/index.ejs', { quizes: quizes});
-	})
+	}).catch(function(error){ next(error);});
 };
 
 
@@ -13,7 +23,7 @@ exports.index = function(req, res) {
 // GET /quizes/question
 exports.show = function(req, res){
 	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		res.render('quizes/show',{quiz: quiz})
+		res.render('quizes/show',{quiz: req.quiz})
 	})
 };
 
@@ -21,12 +31,12 @@ exports.show = function(req, res){
 // GET /quizes/answer
 exports.answer = function(req, res){
 	models.Quiz.findById(req.params.quizId).then(function(quiz){
-		if(req.query.respuesta === quiz.respuesta){
+		if(req.query.respuesta === req.quiz.respuesta){
 			res.render('quizes/answer',
 				{ quiz: quiz, respuesta: 'Correcto'});
 		}else{
 			res.render('quizes/answer',
-				{ quiz: quiz, respuesta: 'Incorrecto'});
+				{ quiz: req.quiz, respuesta: 'Incorrecto'});
 		}
 		
 	})
