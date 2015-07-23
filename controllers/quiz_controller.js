9c7dfb1fe5372b0451256a85c1243f53117c5692
David Huerta <dhuerta23@gmail.com>
2015-07-23 11:34:55 +0200

    Editar Preguntas

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -80,3 +80,27 @@ exports.create = function(req, res){
 		
 						);
 };
+
+
+// GET quizes/:id/edit
+exports.edit = function(req, res){
+	var quiz = req.quiz;	
+	res.render('quizes/edit',{quiz: quiz, errors: []});
+};
+
+
+exports.update = function(req, res){
+	req.quiz.pregunta = req.body.quiz.pregunta;
+	req.quiz.respuesta = req.body.quiz.respuesta;
+
+	req.quiz.validate().then(
+		function(err){
+			if(err){
+				res.render('quizes/edit', {quiz: req.quiz, erros: err.errors});
+			}else{
+				req.quiz
+				.save( {fields: ["preunta", "respuesta"]})
+				.then( function(){ res.redirect('/quizes');});
+			}
+		});
+};
